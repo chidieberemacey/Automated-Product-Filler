897a280f3dfa7167ee53cd8ee61d1760272333b1
Chidiebere Macey <egbomaceyskyla@gmail.com>
2023-08-27 23:45:11 +0100

    first commit

---
+++ js/orders.js
@@ -0,0 +1,44 @@
+const Orders =[
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Drone',
+        productNumber: '86527',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '83231',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '89695',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '79273',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '30093',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
